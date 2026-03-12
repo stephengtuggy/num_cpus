92d04e230e6e1de206aaf1df93f3075be54c9728
Stephen G Tuggy <sgt@stephengtuggy.com>
2026-03-11 23:07:59 -0700

    Still more refinements to num_cpus and num_cpus.sh. Specifically, pipe any error output to /dev/null

--- num_cpus.sh
+++ num_cpus.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-grep -c 'cpu[0-9]' /proc/stat || getconf _NPROCESSORS_ONLN || getconf NPROCESSORS_ONLN
+grep -c 'cpu[0-9]' /proc/stat 2>/dev/null || getconf _NPROCESSORS_ONLN 2>/dev/null || getconf NPROCESSORS_ONLN 2>/dev/null || echo 1
